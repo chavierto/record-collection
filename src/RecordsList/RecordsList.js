214973883a98681e3079c2b8358f1f3bbdf6e0f3
chavierto <hola@xavierlujan.com>
2020-09-13 19:37:00 -0400

    Branched into dev

--- src/RecordsList/RecordsList.js
+++ src/RecordsList/RecordsList.js
@@ -1,4 +1,31 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './RecordsList.css';
+import RecordCard from '../RecordCard/RecordCard';
 import axios from 'axios';
 import Modal from 'react-bootstrap/Modal';
+
+function RecordsList() {
+	const [data, setData] = useState({
+		records: {},
+	});
+
+	useEffect(async () => {
+		const result = await axios(
+			'https://record-collection-be-xl.herokuapp.com/'
+		);
+
+		setData(result.data);
+	}, []);
+
+	return (
+		<ul>
+			{data.records.map((i) => (
+				<li key={i.ID}>
+					<a href={i.url}>{i.title}</a>
+				</li>
+			))}
+		</ul>
+	);
+}
+
+export default RecordsList;
